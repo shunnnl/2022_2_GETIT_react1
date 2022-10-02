18f9a76c48cac739883b81a51ea160027837c1c0
shunnnl <103299126+shunnnl@users.noreply.github.com>
2022-10-02 22:27:24 +0900

    Update index.js

--- 02/src/index.js
+++ 02/src/index.js
@@ -9,11 +9,24 @@ import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/*
+setInterval(()=>
+{
+  root.render(
+    <React.StrictMode>
+      <Clock/>
+    </React.StrictMode>
+  );
+},1000)
+*/
+
 root.render(
   <React.StrictMode>
     <CommentList/>
   </React.StrictMode>
 )
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
