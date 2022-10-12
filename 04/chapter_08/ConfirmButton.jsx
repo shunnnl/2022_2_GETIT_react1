f0aa44a888aca93ac3e3dc6f859748d85e6b8775
shunnnl <103299126+shunnnl@users.noreply.github.com>
2022-10-12 14:57:38 +0900

    Add files via upload

---
+++ 04/chapter_08/ConfirmButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {useState} from "react";
+
+function ComfirmButton(props){
+    const[isConfirmed,setIsConfirmed]=useState(false);
+
+    const handleConfirm=()=>{
+        setIsConfirmed((prevIsConfirmed)=>!prevIsConfirmed);
+    };
+
+    return(
+        <button onClick={handleConfirm} disabled={isConfirmed}>
+            {isConfirmed?"확인됨":"확인하기"}
+        </button>
+    );
+}
+export default ConfirmButton;
